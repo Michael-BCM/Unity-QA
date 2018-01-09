040e1a9a98a9472bbb0db5fc512619c5fa2e5dfe
Michael-BCM <michael@bluecarbonmedia.co.uk>
2018-01-09 18:43:02 +0000

    Initial commit

---
+++ Dodge Homing Missile/Assets/Cannon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private GameObject missile;
+
+    [SerializeField]
+    private Transform instantiationPoint;
+
+    private void Update()
+    {
+        if (missile == null)
+        {
+            missile = Instantiate(prefab, instantiationPoint.position, Quaternion.Euler(-90, 0, 0));
+        }
+    }
+}
---
+++ Dodge Homing Missile/Assets/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 10;
+
+	private void Update ()
+    {
+        Vector3 movementVector = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0), 1);
+        
+        transform.Translate(movementVector * Time.deltaTime * movementSpeed);
+	}
+
+}
